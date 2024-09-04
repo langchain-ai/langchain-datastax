7af9eeea3a41c35a4fa911aa9c6a773e1fc23cb7
Ryan Michael <kerinin@gmail.com>
2024-09-04 15:26:58 -0400

    lint fixes

--- libs/astradb/langchain_astradb/__init__.py
+++ libs/astradb/langchain_astradb/__init__.py
@@ -5,9 +5,9 @@ from astrapy.info import CollectionVectorServiceOptions
 from langchain_astradb.cache import AstraDBCache, AstraDBSemanticCache
 from langchain_astradb.chat_message_histories import AstraDBChatMessageHistory
 from langchain_astradb.document_loaders import AstraDBLoader
+from langchain_astradb.graph_vectorstores import AstraDBGraphVectorStore
 from langchain_astradb.storage import AstraDBByteStore, AstraDBStore
 from langchain_astradb.vectorstores import AstraDBVectorStore
-from langchain_astradb.graph_vectorstores import AstraDBGraphVectorStore
 
 __all__ = [
     "AstraDBByteStore",
