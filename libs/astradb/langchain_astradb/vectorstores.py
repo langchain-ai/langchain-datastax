fcfe42de83329115a5397ca52669bbfb64be6c2f
Stefano Lottini <stefano.lottini@ibm.com>
2025-10-15 16:15:58 +0200

    method internal references; also lockfile

--- libs/astradb/langchain_astradb/vectorstores.py
+++ libs/astradb/langchain_astradb/vectorstores.py
@@ -885,7 +885,7 @@ class AstraDBVectorStore(VectorStore):
             collection_lexical: configuring a lexical analyzer is necessary to run
                 lexical and hybrid searches. This parameter can be a string or dict,
                 which is then passed as-is for the "analyzer" field of a
-                createCollection's "$lexical.analyzer" value, or a ready-made
+                createCollection's `"$lexical.analyzer"` value, or a ready-made
                 astrapy `CollectionLexicalOptions` object.
             hybrid_search: whether similarity searches should be run as Hybrid searches
                 or not. Values are DEFAULT, ON or OFF. In case of DEFAULT, searches
@@ -904,13 +904,13 @@ class AstraDBVectorStore(VectorStore):
                 If a `HybridLimitFactorPrescription` is provided (see the class
                 docstring for details), separate factors are applied to the vector
                 and the lexical subsearches. Alternatively, a simple dictionary
-                with keys "$lexical" and "$vector" achieves the same effect.
+                with keys `"$lexical"` and `"$vector"` achieves the same effect.
 
         Raises:
             ValueError: if the parameters are inconsistent or invalid.
 
         Note:
-            For concurrency in synchronous :meth:`~add_texts`:, as a rule of thumb,
+            For concurrency in synchronous [add_texts](langchain_astradb.AstraDBVectorStore.add_texts):, as a rule of thumb,
             on a typical client machine it is suggested to keep the quantity
             bulk_insert_batch_concurrency * bulk_insert_overwrite_concurrency
             much below 1000 to avoid exhausting the client multithreading/networking
@@ -924,7 +924,7 @@ class AstraDBVectorStore(VectorStore):
             depending on both the machine/network specs and the expected workload
             (specifically, how often a write is an update of an existing id).
             Remember you can pass concurrency settings to individual calls to
-            :meth:`~add_texts` and :meth:`~add_documents` as well.
+            [add_texts](langchain_astradb.AstraDBVectorStore.add_texts) and [add_documents](langchain_astradb.AstraDBVectorStore.add_documents) as well.
         """
         # general collection settings
         self.collection_name = collection_name
@@ -1465,7 +1465,7 @@ class AstraDBVectorStore(VectorStore):
         """Completely delete the collection from the database.
 
         Completely delete the collection from the database (as opposed
-        to :meth:`~clear`, which empties it only).
+        to [clear](langchain_astradb.AstraDBVectorStore.clear), which empties it only).
         Stored data is lost and unrecoverable, resources are freed.
         Use with caution.
         """
@@ -1476,7 +1476,7 @@ class AstraDBVectorStore(VectorStore):
         """Completely delete the collection from the database.
 
         Completely delete the collection from the database (as opposed
-        to :meth:`~aclear`, which empties it only).
+        to [aclear](langchain_astradb.AstraDBVectorStore.aclear), which empties it only).
         Stored data is lost and unrecoverable, resources are freed.
         Use with caution.
         """
@@ -3869,7 +3869,7 @@ class AstraDBVectorStore(VectorStore):
             metadatas: metadata dicts for the texts.
             ids: ids to associate to the texts.
             **kwargs: you can pass any argument that you would
-                to :meth:`~add_texts` and/or to the
+                to [add_texts](langchain_astradb.AstraDBVectorStore.add_texts) and/or to the
                 `AstraDBVectorStore` constructor (see these methods for
                 details). These arguments will be
                 routed to the respective methods as they are.
@@ -3913,7 +3913,7 @@ class AstraDBVectorStore(VectorStore):
             metadatas: metadata dicts for the texts.
             ids: ids to associate to the texts.
             **kwargs: you can pass any argument that you would
-                to :meth:`~aadd_texts` and/or to the `AstraDBVectorStore`
+                to [aadd_texts](langchain_astradb.AstraDBVectorStore.aadd_texts) and/or to the `AstraDBVectorStore`
                 constructor (see these methods for details). These arguments
                 will be routed to the respective methods as they are.
 
@@ -3949,13 +3949,13 @@ class AstraDBVectorStore(VectorStore):
     ) -> AstraDBVectorStore:
         """Create an Astra DB vectorstore from a document list.
 
-        Utility method that defers to :meth:`from_texts` (see that one).
+        Utility method that defers to [from_texts](langchain_astradb.AstraDBVectorStore.from_texts) (see).
 
         Args:
             documents: a list of `Document` objects for insertion in the store.
             embedding: the embedding function to use in the store.
             **kwargs: you can pass any argument that you would
-                to :meth:`~add_texts` and/or to the
+                to [add_texts](langchain_astradb.AstraDBVectorStore.add_texts) and/or to the
                 `AstraDBVectorStore` constructor (see these methods for
                 details). These arguments will be
                 routed to the respective methods as they are.
@@ -3998,13 +3998,13 @@ class AstraDBVectorStore(VectorStore):
     ) -> AstraDBVectorStore:
         """Create an Astra DB vectorstore from a document list.
 
-        Utility method that defers to :meth:`afrom_texts` (see that one).
+        Utility method that defers to [afrom_texts](langchain_astradb.AstraDBVectorStore.afrom_texts) (see that one).
 
         Args:
             documents: a list of `Document` objects for insertion in the store.
             embedding: the embedding function to use in the store.
             **kwargs: you can pass any argument that you would
-                to :meth:`~aadd_texts` and/or to the
+                to [aadd_texts](langchain_astradb.AstraDBVectorStore.aadd_texts) and/or to the
                 `AstraDBVectorStore` constructor (see these methods for
                 details). These arguments will be
                 routed to the respective methods as they are.
